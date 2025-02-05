5538de1128b602c4c474b226addfa627819d00f0
sanara <sanara@cpqd.com.br>
2025-02-05 17:00:41 -0300

    organizando repositório

---
+++ Curso React_Native 1/src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor('#DA552F'); //somente android
+StatusBar.setBarStyle('light-content');  //somente ios
---
+++ Curso React_Native 1/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Routes from "./routes";
+import "./config/StatusBarConfig";
+
+
+const App = () => <Routes />;
+
+export default App; 
+
+//plugin para acessar dados de uma API : yarn add axios
